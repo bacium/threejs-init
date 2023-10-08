a50dfa10bd227936a9b23bed886d83469c688ee4
bacium <bacium_dc@163.com>
2023-10-08 17:22:35 +0800

    添加轨道控制器

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import * as THREE from "three"
 
+// 导入轨道控制器
+import { OrbitControls } from "three/addons/controls/OrbitControls.js"
 // 创建场景
 const scene = new THREE.Scene()
 // 创建相机
@@ -17,6 +19,14 @@ renderer.setSize(window.innerWidth, window.innerHeight)
 // 把创建的渲染器放置到页面上渲染出来
 document.body.appendChild(renderer.domElement)
 
+// 轨道控制器
+const controls = new OrbitControls(camera, renderer.domElement)
+// 开启旋转阻尼
+controls.enableDamping = true
+// 设置阻尼系数
+controls.dampingFactory = 0.01
+// controls.autoRotate = true
+
 // 创建几何体
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 // 创建材质
@@ -28,14 +38,21 @@ const cube = new THREE.Mesh(geometry, material)
 scene.add(cube)
 // 相机位置
 camera.position.z = 5
+camera.position.y = 2
+camera.position.x = 2
 camera.lookAt(0, 0, 0)
+
+// 添加轨道
+const axesHelper = new THREE.AxesHelper(5)
+scene.add(axesHelper)
 
 // 持续变化-定义一个渲染函数
 function animate() {
+  controls.update()
   requestAnimationFrame(animate)
   // 旋转
-  cube.rotation.x += 0.01
-  cube.rotation.y += 0.01
+  // cube.rotation.x += 0.01
+  // cube.rotation.y += 0.01
   // 渲染
   renderer.render(scene, camera)
 }
